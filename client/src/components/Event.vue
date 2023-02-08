6a8f2a09afbe3a3bba88903119ac57d430e186d9
abdulQudoos7 <abdul.qudoos@southvillesol.com>
2023-02-08 14:30:27 +0500

    Event-practice

---
+++ client/src/components/Event.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+    <h1>Events in Vue </h1>
+    <button v-on:click="getMsg()">Click Me</button>
+
+    <button @click="getMsg()">Using shorten @ </button>
+
+    <button v-on:click="getMsg1('Passing params Alert')">Passing params</button>
+
+</div>
+</template>
+<script>
+import { registerRuntimeCompiler } from 'vue';
+
+export default{
+    name: 'Event',
+    data() {
+    },
+    methods: {
+        getMsg(){
+            alert("hello world")
+        },
+        getMsg1(text){
+            alert(text)
+        }
+    }
+}
+</script>
+<style>
+</style>
