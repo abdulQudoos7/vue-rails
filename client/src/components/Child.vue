f8dedd203839df09eb8cdf638e65efd238d0d6c7
abdulQudoos7 <abdul.qudoos@southvillesol.com>
2023-02-09 11:26:15 +0500

    pass-data-parent-to-child-component

---
+++ client/src/components/Child.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Child Component</h1>
+    <p>Child gets from parent => {{ text }}</p>
+    <p>Child gets data from parent => {{ user }} </p>
+    <button @click="sendData(2,2)">Click me</button>
+
+</template>
+<script>
+export default{
+    name: 'Child',
+    props:{
+        text: String,
+        user: Object,
+        sendData: Function,
+    }
+}
+</script>
+<style></style>
---
+++ client/src/components/Parent.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>Parent Component</h1>
+    <Child text="text form parent component" :user="user" :sendData="sendData" />
+</template>
+<script >
+import Child from './Child.vue';
+
+export default{
+    name: "Parent",
+    components: { Child },
+    data(){
+        return{
+            user:{name:'Abdul Qudoos', email: 'abdulqudoos@test.com'}
+        }
+    },
+    methods: {
+        sendData(a,b){
+            console.log(a+b);
+        }
+    }
+}
+</script>
+<style></style>
