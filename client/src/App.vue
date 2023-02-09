5f750fcb1576aa771d5754068dcd4a84e7a2096f
abdulQudoos7 <abdul.qudoos@southvillesol.com>
2023-02-09 13:53:26 +0500

    Resue component

--- client/src/App.vue
+++ client/src/App.vue
@@ -8,21 +8,23 @@
     <CheckBox /> -->
     <!-- <ConditionalRendering /> -->
     <!-- <ForLoop /> -->
-    <Parent />
+    <!--<Parent />-->
+    <ParentToChild />
 
   </div>
 </template>
 
 <script setup>
-import Posts from './components/Post.vue'
-import Interpolation from "./components/Interpolation.vue";
-import Event from './components/Event.vue';
-import TwoWayBinding from './components/TwoWayBinding.vue';
-import SignIn from './components/SignIn.vue';
-import CheckBox from './components/Checkbox.vue'
-import ConditionalRendering from './components/ConditionalRendering.vue';
-import ForLoop from './components/ForLoop.vue';
-import Parent from './components/Parent.vue';
+import Posts from './components/yt_practice/Post.vue'
+import Interpolation from "./components/yt_practice/Interpolation.vue";
+import Event from './components/yt_practice/Event.vue';
+import TwoWayBinding from './components/yt_practice/TwoWayBinding.vue';
+import SignIn from './components/yt_practice/SignIn.vue';
+import CheckBox from './components/yt_practice/Checkbox.vue'
+import ConditionalRendering from './components/yt_practice/ConditionalRendering.vue';
+import ForLoop from './components/yt_practice/ForLoop.vue';
+import Parent from './components/yt_practice/Parent.vue';
+import ParentToChild from "./components/yt_practice/ParentToChild.vue";
 </script>
 
 <style scoped>
---
+++ client/src/components/yt_practice/ParentToChild.vue
@@ -0,0 +1,45 @@
+<template>
+  <h1>Parent2</h1>
+  <ul>
+    <li v-for="user in users" :key="user.name" >
+<!--      {{user.name}}-->
+      <Child2 :user="user" :getAlert="getAlert"/>
+
+    </li>
+  </ul>
+
+  <!--  <h4>parent=>{{users.name}}</h4>-->
+
+</template>
+
+<script>
+import Child2 from "./Child2.vue";
+
+export default {
+  name: "ParentToChild",
+  components: {Child2},
+  data() {
+    return {
+      users: [
+        {name: "user1", email: "user1@gmail.com"},
+        {name: "user2", email: "user2@gmail.com"},
+        {name: "user3", email: "user3@gmail.com"},
+        {name: "user4", email: "user4@gmail.com"},
+        {name: "user8", email: "user8@gmail.com"},
+        {name: "user7", email: "user7@gmail.com"},
+        {name: "user6", email: "user6@gmail.com"},
+        {name: "user5", email: "user5@gmail.com"},
+      ]
+    }
+  },
+  methods: {
+    getAlert(name){
+      alert("Hello "+name)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
